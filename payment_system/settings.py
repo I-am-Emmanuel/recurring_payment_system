7788cc823eec9b57c6f499b09b2de97874db60c0
hemakulate28 <olaifaemmanueloluwatobiloba@gmail.com>
2023-05-03 08:45:21 +0100

    correction from the .txt

--- payment_system/settings.py
+++ payment_system/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = env('SECRET_KEY')
 # PAYSTACK_SECRET_KEY = env('PAYSTACK_SECRET_KEY')
--- services/payment/views.py
+++ services/payment/views.py
@@ -199,18 +199,17 @@ def stop_recurring_payment(request, pk=None):
 
 
 class CustomerPaymentHistory(ListAPIView):
-    # permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminUser]
     queryset = PaymentModel.objects.all()
     serializer_class = PaymentSystemSerializer
     
     
-    @action(detail=True, permission_classes=[ViewCustomerPaymentHistoryPermission])
+    @action(detail=False, method=['GET'], permission_classes=[ViewCustomerPaymentHistoryPermission])
     def history(self, request, pk):
         # Get the customer
-        customer = PaymentModel.objects.get(pk=pk)
-        # Filter the payments by the customer
-        payments = PaymentModel.objects.filter(customer=customer.id)
+        customer = PaymentModel.objects.get(pk=request.user.id)
+        if request.method == 'GET':
         # Serialize the payments
-        serializer = self.serializer_class(payments, many=True)
-        return Response(serializer.data)
+            serializer = self.serializer_class(customer, many=True)
+            return Response(serializer.data)
 
